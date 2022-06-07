6f838731761a073c07b9e31789e2f9401784e7b7
Daniel Tovar Perdomo <danieltoper7@gmail.com>
2022-06-07 17:56:03 -0500

    agregando saltos de nevagacion

--- js/index.js
+++ js/index.js
@@ -3,24 +3,27 @@ var cabecera = document.querySelector(".contenedor-principal");
 var menutoggler = document.querySelector(".fas");
 var listaNavegacion = document.querySelector(".lista-navegacion");
 
-document.addEventListener("scroll", function(){
+document.addEventListener("scroll", function(event){
+    event.preventDefault();
 
     var scrollVertical = window.pageYOffset/1000;
-    
+    console.log(scrollVertical);
     if(scrollVertical < 0.5){
         cabecera.style.opacity = 0;
-    }else if(scrollVertical > 0.5 && scrollVertical < 0.6){
-        scrollVertical = scrollVertical - 0.4;
+    }
+    else if(scrollVertical > 0.5 && scrollVertical < 0.6){
+        scrollVertical = scrollVertical - 0.25;
         cabecera.style.opacity = scrollVertical;
-    }else if(scrollVertical > 0.6 && scrollVertical < 0.65){
-        scrollVertical = scrollVertical - 0.1;
+    }else if(scrollVertical > 0.6 && scrollVertical < 0.7){
+        scrollVertical = scrollVertical - 0.2;
         cabecera.style.opacity = scrollVertical;
-    }else if(scrollVertical > 0.65){
-        scrollVertical = scrollVertical;
+    }else if(scrollVertical > 0.7 && scrollVertical < 0.8){
+        scrollVertical = scrollVertical + 0.1;
+        cabecera.style.opacity = scrollVertical;
+    }else if (scrollVertical > 0.8 && scrollVertical < 1){
+        scrollVertical = scrollVertical + 0.2;
         cabecera.style.opacity = scrollVertical;
     }
-    listaNavegacion.style.display = "none";
-    estatus = false;
 });
 
 menutoggler.addEventListener("click", function(event){
